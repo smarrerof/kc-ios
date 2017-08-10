d1a14b39b1b036d6264256a26737241cd026c70e
Sergio Marrero <smarrerof@gmail.com>
2017-08-10 06:27:18 +0100

    Updated episode tests to use season repository

--- WesterosTests/EpisodeTests.swift
+++ WesterosTests/EpisodeTests.swift
@@ -11,9 +11,12 @@ import XCTest
 
 class EpisodeTests: XCTestCase {
     
+    var seasons: Seasons!
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        seasons = Repository.local.seasons
     }
     
     override func tearDown() {
@@ -30,10 +33,9 @@ class EpisodeTests: XCTestCase {
     
     func testEpisodeCustomStringConvertible() {
         
-        let season = Season(name: "Season 1", releaseDate: Date.fromString(date: "2011/05/09")!, poster: #imageLiteral(resourceName: "season1.jpg"), overview: "Trouble is brewing in the Seven Kingdoms of Westeros. For the driven inhabitants of this visionary world, control of Westeros' Iron Throne holds the lure of great power. But in a land where the seasons can last a lifetime, winter is coming...and beyond the Great Wall that protects them, an ancient evil has returned. In Season One, the story centers on three primary areas: the Stark and the Lannister families, whose designs on controlling the throne threaten a tenuous peace; the dragon princess Daenerys, heir to the former dynasty, who waits just over the Narrow Sea with her malevolent brother Viserys; and the Great Wall--a massive barrier of ice where a forgotten danger is stirring.")
-        
-        let episode = Episode(name: "Winter Is Coming", overview: "Jon Arryn, the Hand of the King, is dead. King Robert Baratheon plans to ask his oldest friend, Eddard Stark, to take Jon's place. Across the sea, Viserys Targaryen plans to wed his sister to a nomadic warlord in exchange for an army.", airDate: Date.fromString(date: "2011/04/17")!, poster:#imageLiteral(resourceName: "episode1x01.jpg"), season: season)
-        
+        let season = seasons[0]
+        let episode = season.sortedEpisodes()[0]
+
         XCTAssertEqual(episode.description, "Episode: \(episode.name)")
         
         XCTAssertNotEqual(episode.description, episode.name)
@@ -41,35 +43,34 @@ class EpisodeTests: XCTestCase {
     
     func testEpisodeEquatable() {
         
-        let season = Season(name: "Season 1", releaseDate: Date.fromString(date: "2011/05/09")!, poster: #imageLiteral(resourceName: "season1.jpg"), overview: "Trouble is brewing in the Seven Kingdoms of Westeros. For the driven inhabitants of this visionary world, control of Westeros' Iron Throne holds the lure of great power. But in a land where the seasons can last a lifetime, winter is coming...and beyond the Great Wall that protects them, an ancient evil has returned. In Season One, the story centers on three primary areas: the Stark and the Lannister families, whose designs on controlling the throne threaten a tenuous peace; the dragon princess Daenerys, heir to the former dynasty, who waits just over the Narrow Sea with her malevolent brother Viserys; and the Great Wall--a massive barrier of ice where a forgotten danger is stirring.")
+        let season = seasons[0]
         
-        let lhs = Episode(name: "Winter Is Coming", overview: "Jon Arryn, the Hand of the King, is dead. King Robert Baratheon plans to ask his oldest friend, Eddard Stark, to take Jon's place. Across the sea, Viserys Targaryen plans to wed his sister to a nomadic warlord in exchange for an army.", airDate: Date.fromString(date: "2011/04/17")!, poster:#imageLiteral(resourceName: "episode1x01.jpg"), season: season)
+        let lhs = season.sortedEpisodes()[0]
         
         var rhs = Episode(name: "Winter Is Coming", overview: "Jon Arryn, the Hand of the King, is dead. King Robert Baratheon plans to ask his oldest friend, Eddard Stark, to take Jon's place. Across the sea, Viserys Targaryen plans to wed his sister to a nomadic warlord in exchange for an army.", airDate: Date.fromString(date: "2011/04/17")!, poster:#imageLiteral(resourceName: "episode1x01.jpg"), season: season)
         
         XCTAssertEqual(lhs, rhs)
         
-        rhs = Episode(name: "The Kingsroad", overview: "While Bran recovers from his fall, Ned takes only his daughters to Kings Landing. Jon Snow goes with his uncle Benjen to The Wall. Tyrion joins them.", airDate: Date.fromString(date: "2011/04/24")!, poster:#imageLiteral(resourceName: "episode1x02.jpg"), season: season)
+        rhs = season.sortedEpisodes()[1]
         
         XCTAssertNotEqual(lhs, rhs)
     }
     
     func testEpisodeHashable() {
         
-        let season = Season(name: "Season 1", releaseDate: Date.fromString(date: "2011/05/09")!, poster: #imageLiteral(resourceName: "season1.jpg"), overview: "Trouble is brewing in the Seven Kingdoms of Westeros. For the driven inhabitants of this visionary world, control of Westeros' Iron Throne holds the lure of great power. But in a land where the seasons can last a lifetime, winter is coming...and beyond the Great Wall that protects them, an ancient evil has returned. In Season One, the story centers on three primary areas: the Stark and the Lannister families, whose designs on controlling the throne threaten a tenuous peace; the dragon princess Daenerys, heir to the former dynasty, who waits just over the Narrow Sea with her malevolent brother Viserys; and the Great Wall--a massive barrier of ice where a forgotten danger is stirring.")
-        
-        let episode = Episode(name: "Winter Is Coming", overview: "Jon Arryn, the Hand of the King, is dead. King Robert Baratheon plans to ask his oldest friend, Eddard Stark, to take Jon's place. Across the sea, Viserys Targaryen plans to wed his sister to a nomadic warlord in exchange for an army.", airDate: Date.fromString(date: "2011/04/17")!, poster:#imageLiteral(resourceName: "episode1x01.jpg"), season: season)
+        let season = seasons[0]
+        let episode = season.sortedEpisodes()[0]
         
         XCTAssertNotNil(episode.hashValue)
     }
     
     func testEpisodeComparison() {
         
-        let season = Season(name: "Season 1", releaseDate: Date.fromString(date: "2011/05/09")!, poster: #imageLiteral(resourceName: "season1.jpg"), overview: "Trouble is brewing in the Seven Kingdoms of Westeros. For the driven inhabitants of this visionary world, control of Westeros' Iron Throne holds the lure of great power. But in a land where the seasons can last a lifetime, winter is coming...and beyond the Great Wall that protects them, an ancient evil has returned. In Season One, the story centers on three primary areas: the Stark and the Lannister families, whose designs on controlling the throne threaten a tenuous peace; the dragon princess Daenerys, heir to the former dynasty, who waits just over the Narrow Sea with her malevolent brother Viserys; and the Great Wall--a massive barrier of ice where a forgotten danger is stirring.")
+        let season = seasons[0]
         
-        let lhs = Episode(name: "Winter Is Coming", overview: "Jon Arryn, the Hand of the King, is dead. King Robert Baratheon plans to ask his oldest friend, Eddard Stark, to take Jon's place. Across the sea, Viserys Targaryen plans to wed his sister to a nomadic warlord in exchange for an army.", airDate: Date.fromString(date: "2011/04/17")!, poster:#imageLiteral(resourceName: "episode1x01.jpg"), season: season)
+        let lhs = season.sortedEpisodes()[0]
         
-        let rhs = Episode(name: "The Kingsroad", overview: "While Bran recovers from his fall, Ned takes only his daughters to Kings Landing. Jon Snow goes with his uncle Benjen to The Wall. Tyrion joins them.", airDate: Date.fromString(date: "2011/04/24")!, poster:#imageLiteral(resourceName: "episode1x02.jpg"), season: season)
+        let rhs = season.sortedEpisodes()[1]
         
         XCTAssertLessThan(lhs, rhs)
     }
